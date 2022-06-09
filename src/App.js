f1da7601ba19fcb34fd29af46082c628d4ba3ba0
KELLY LUU <kellyluuu@gmail.com>
2022-06-08 21:38:48 -0500

    update with google login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Main from './components/Main';
 import Nav from './components/Nav';
-//import Google from './Google';
+import Google from './Google';
 
 
 
 function App() {
+  
   return (
     <div className="App">
       <Nav />
+      <Google />
       <Main />
-
-
     </div>
   );
 }
--- src/Google.js
+++ src/Google.js
@@ -1,69 +1,59 @@
-import { GoogleLogin } from 'react-google-login';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import jwt_decode from "jwt-decode"
 
 
 
 
 
 function Google() {
+  const [ user, setUser] = useState({})
 
 
-    const [loginData, setLoginData] = useState(
-        localStorage.getItem('loginData')
-          ? JSON.parse(localStorage.getItem('loginData'))
-          : null
-      );
-    
-      const handleFailure = (result) => {
-        console.log(result)
-      };
-    
-      const handleLogin = async (googleData) => {
-        const res = await fetch('/api/google-login', {
-          method: 'POST',
-          body: JSON.stringify({
-            token: googleData.tokenId,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-    
-        const data = await res.json();
-        setLoginData(data);
-        localStorage.setItem('loginData', JSON.stringify(data));
-      };
-      const handleLogout = () => {
-        localStorage.removeItem('loginData');
-        setLoginData(null);
-      };
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token: " + response.credential)
+    let userObject = jwt_decode(response.credential)
+    console.log(userObject)
+    setUser(userObject)
+    document.getElementById('signInDiv').hidden = true
+  }
 
-      return (
-        <div className="App">
-          <header className="App-header">
-            <h1>React Google Login App</h1>
-            <div>
-              {
-              loginData ? (
-              <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}>Logout</button>
-             </div>
-              ) 
-              : 
-              (
-                <GoogleLogin
-                  clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                  buttonText="Log in with Google"
-                  onSuccess={handleLogin}
-                  onFailure={handleFailure}
-                  cookiePolicy={'single_host_origin'}
-                ></GoogleLogin>
-              )}
-            </div>
-          </header>
-        </div>
-      );
+  function handleSignOut(event){
+    setUser({})
+    document.getElementById("signInDiv").hidden = false
+  }
+
+  useEffect(()=>{
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+    google.accounts.id.prompt()
+  },[])
+// IF NO USER SHOW SIGN IN BUTTON 
+// IF WE HAVE USER : SHOW THE LOG OUT BUTTON 
+
+  return (
+    <div className='App'>
+      <div id="signInDiv" alt={user.name}></div>
+      { Object.keys(user).length !=0 &&
+      <button onClick={(e)=>handleSignOut(e)}>Sign Out</button>
+      }
+      { user &&
+      <div>
+       <img src ={user.picture}></img>
+       <h3>{user.name}</h3>
+       </div>
+        }
+    </div>
+  )
+
+
     }
     
 export default Google;
