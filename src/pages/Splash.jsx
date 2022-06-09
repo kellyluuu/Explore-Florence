937399e20322c43fba2595ce894745fd8b1ac3ac
chess2022 <lakeshore.hnh@gmail.com>
2022-06-08 17:27:46 -0700

    Adds explore button to splash page & updates scss

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Splash() {
   return (
@@ -10,7 +11,15 @@ export default function Splash() {
       />
       <div id="splash--text">
         <h5 className="splash--sectionTitle">Explore Florence</h5>
-        <p className="splash--sectionDesc">Florence, the capital of Italy's Tuscany region, is home to many masterpieces of Renaissance art and architecture. Surrounded by gently rolling hills covered with farms, vineyards, and orchards, there is no shortage of activities for you to enjoy during your stay.</p>
+        <p className="splash--sectionDesc">
+          Florence, the capital of Italy's Tuscany region, is home to many
+          masterpieces of Renaissance art and architecture. Surrounded by gently
+          rolling hills covered with farms, vineyards, and orchards, there is no
+          shortage of activities for you to enjoy during your stay.
+        </p>
+        <Link to="/activity">
+          <button>EXPLORE</button>
+        </Link>
       </div>
     </div>
   );
