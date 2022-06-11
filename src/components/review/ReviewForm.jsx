92f4da86ba390af29b6150954fd5f994e1607f5f
Fernando Galvan <n.brighter00@gmail.com>
2022-06-11 12:00:51 -0500

    adding changes

---
+++ src/components/review/ReviewForm.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import RatingSelect from "./RatingSelect";
+import Button from "./Button";
+import Google from "../../Google";
+import { useParams } from 'react-router-dom'
+
+// createReview is passed down from Main.JSX
+
+export default function ReviewForm({ createReview }) {
+  const id = useParams()
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(4);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const [activityId, setActivityId] = useState(id)
+
+
+  const handleChange = (event) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 1) {
+      setMessage("Text must be at least 4 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setActivityId(id)
+      setBtnDisabled(false);
+    }
+    setText(event.target.value);
+  };
+
+//   const handleChange = (event) => {
+//     setReview((prevState) => ({
+//       ...prevState,
+//       [event.target.name]: event.target.value,
+//     }));
+//   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (text.trim().length > 3) {
+        const newReview = {
+        text,
+        rating
+      
+        }
+        createReview(newReview);
+        setText("")
+        }
+    }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div>
+          <input type="hidden" value = {activityId}/>
+          <textarea
+            onChange={handleChange}
+            type="text"
+            placeholder="Write a review..."
+            value={text}
+            className="review--input-box"
+            rows="6"
+            cols="60"
+          />
+          <div className="review--button">
+            <div>
+              <Google />
+            </div>
+            <div>
+              <Button type="submit" isDisabled={btnDisabled}>
+                ADD REVIEW
+              </Button>
+            </div>
+          </div>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </div>
+  );
+}
